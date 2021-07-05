abdda8d195fec703d8f6f75a72ccd0e9a0a6538f
Gabriel <gabrielvt6@gmail.com>
2021-07-05 13:03:41 -0700

    Add next piece component

---
+++ src/components/NextPiece.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class NextPiece extends React.Component {
+  renderSquare(piece, index) {
+    return <div className="square" key={index}>{piece.value}</div>;
+  }
+
+  render() {
+    return (
+      <div className="incoming">
+        <div className="current">
+          Current: {this.renderSquare(this.props.piece, 0)}
+        </div>
+        <div className="next">
+          Next: {this.renderSquare(this.props.nextPiece, 1)}
+        </div>
+      </div>
+    );
+  };
+};
